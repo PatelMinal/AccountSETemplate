f9f098f7d68db15f1691109a6f12bd3e813a38cb
Admin <Admin@STUDENT18>
2019-01-31 15:36:55 +0000

    Database Repository

---
+++ src/main/java/com/qa/persistence/repository/AccountDBRepositroy.java
@@ -0,0 +1,80 @@
+package com.qa.persistence.repository;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import com.qa.persistance.util.JSONUtil;
+import com.qa.persistence.domain.Account;
+
+
+
+@Transactional(SUPPORTS)
+@Default
+public class AccountDBRepositroy implements AccountRepository {
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil util;
+	
+
+	
+	@Override
+	@Transactional(REQUIRED)
+	public String getAllAccounts() {
+		Query getaccount = manager.createQuery("SELECT from ACCOUNT a", Account.class);
+		Collection<Account> accounts = (Collection<Account>) getaccount.getResultList();
+		return util.getJSONForObject(getaccount);
+	}
+
+	@Override
+	@Transactional(REQUIRED)
+	public String createAccount(String account) {
+		Account aAccount = util.getObjectForJSON(account, Account.class);
+		manager.persist(aAccount);
+		return "{\"Message\": \"Account Sucessfully Added\"}";
+	}
+
+	@Override
+	@Transactional(REQUIRED)
+	public String deleteAccount(Long id) {
+		Account accountDB = findaAccount(id);
+		if (accountDB != null) {
+			manager.remove(accountDB);
+		}
+		return "{\"Message\": \"Account Sucessfully Removed\"}";
+	}
+
+	
+	@Override
+	public String updateAccount(Long id, String account) {
+		Account update = util.getObjectForJSON(account, Account.class);
+		manager.createQuery("UPDATE ACCOUNT SET firstName" + update.getFirstName());
+		manager.createQuery("UPDATE ACCOUNT SET lastName" + update.getLastName());
+		manager.createQuery("UPDATE ACCOUNT SET accountnumber" + update.getAccountNumber());
+		
+		return "Updated account with first name: " + update.getFirstName() + ", Last name: " + update.getLastName()
+		+ ", Account Number: " + update.getAccountNumber();
+	}
+
+	@Override
+	public Account findaAccount(Long id) {
+		return manager.find(Account.class, id);
+	}
+
+	public String findAccount(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
