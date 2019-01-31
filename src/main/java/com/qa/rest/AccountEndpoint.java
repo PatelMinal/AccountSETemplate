d8791227ea6659c6b2ee8853ab55fb80aa57ccd5
Admin <Admin@STUDENT18>
2019-01-31 15:59:41 +0000

    JAX - RS Implementation

---
+++ src/main/java/com/qa/rest/AccountEndpoint.java
@@ -0,0 +1,49 @@
+package com.qa.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.qa.business.service.AccountService;
+
+@Path("/account")
+
+public class AccountEndpoint {
+	
+	@Inject
+	private AccountService service;
+	
+	@Path("/getallaccounts")
+	
+	@GET
+	@Produces({ "application/json" })
+	public String getAllAccounts() {
+		return service.getAllAccounts();
+	}
+
+	@Path("/createaccount")
+	@POST
+	@Produces({ "application/json" })
+	public String  createAccount(String account) {
+		return service.createAccount(account);
+	}
+
+	@Path("/deleteAccount/{id}")
+	@DELETE
+	@Produces({ "application/json" })
+	public String deleteAccount(@PathParam("id") Long id) {
+		return service.deleteAccount(id);
+	}
+	
+	public void setService(AccountService service) {
+		this.service = service;
+	}
+
+	
+	
+
+}
